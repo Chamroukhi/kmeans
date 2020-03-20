9cf1dc899240ba7d8d712e0a64a4e2fad8f24c0a
Chamroukhi Nawfel <45068882+Chamroukhi@users.noreply.github.com>
2020-03-20 09:54:53 +0100

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -1,18 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x=[[12,34],[56,78],[18,15],[19,30],[34,54],[30,40],[26,34],[8,17],[38,49],[9,29]]
+x=[[12,34,2],[56,78,3],[18,15,32],[19,30,12],[34,54,12],[30,40,9],[26,34,19],[13,8,17],[23,38,49],[32,9,29]]
 
 
 #Début
  
-#========================= initialisation: ===========================#
+#============================ initialisation: ============================#
 
 #nombre de classe
-nombre_classe=2
+nombre_classe=4
 
 #nombre des variables explicatives x1,x2,...,xn
-nombre_feature=2
+nombre_feature=3
 
 #nombre d'observation (les points)
 observation=8
@@ -21,50 +21,55 @@ observation=8
 groupe=[]
 
 #centroide
-c=[[12,34],[56,78]]
+c=[[12,34,2],[56,78,3],[26,34,19],[19,30,12]]
 
-#nombre de groupe = nombre de classe
-for g in range(nombre_classe):
-    groupe.append([])
+if (len(c)<nombre_classe):
+    print("vérifier les centres de classe")
+elif(len(c[0])<nombre_feature):
+    print("vérifier le nombre des features représente le point")
+else:
+#======== creation de groupe (nombre de groupe = nombre de classe) =========#
+    for g in range(nombre_classe):
+        groupe.append([])
 
-#distance euclidienne
-def distanceE(centre,point):
-    """ distance euclidienne """
-    s=0
-    for i in range(nombre_feature):
-        s=s+(pow((point[i]-centre[i]),2))
-    d=np.sqrt(s)
-    return d
+#=========================== distance euclidienne ==========================#
+    def distanceE(centre,point):
+        """ distance euclidienne """
+        s=0
+        for i in range(nombre_feature):
+            s=s+(pow((point[i]-centre[i]),2))
+            d=np.sqrt(s)
+            return d
+        
+#================================ affectation: =============================#
 
-#=================================== affectation: ==========================#
-
-for i in range(observation):
-    distance=[]
-    for k in range(nombre_classe):
-        d=0
-        d=distanceE(c[k], x[i])
-        distance.append(d)
-    classe=min(distance)   
-    for k in range(nombre_classe):
-        if classe == distance[k]:
+    for i in range(observation):
+        distance=[]
+        for k in range(nombre_classe):
+            d=0
+            d=distanceE(c[k], x[i])
+            distance.append(d)
+        classe=min(distance)   
+        for k in range(nombre_classe):
+            if classe == distance[k]:
             
-            groupe[k].append(x[i])
+                groupe[k].append(x[i])
             
-            #recalculer les centroide
-            for j in range(nombre_feature):
-               w=0
-               for l in range(len(groupe[k])):
+#======================== recalculer les centroide =========================#
+                for j in range(nombre_feature):
+                    w=0
+                    for l in range(len(groupe[k])):
                   
-                   w=w+groupe[k][l][j]
+                        w=w+groupe[k][l][j]
                    
-               s=w/len(groupe[k])
-               c[k][j]=s
+                    s=w/len(groupe[k])
+                    c[k][j]=s
 
-#=============================== affichage =============================#             
-for g in range(len(groupe)):
-    print("groupe[",g,"] :",groupe[g])
+#=============================== affichage =================================#             
+    for g in range(len(groupe)):
+        print("groupe[",g,"] :",groupe[g])
 
-print("centroide",c)     
+    print("centroide",c)     
 
 
 
